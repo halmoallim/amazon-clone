d95c29b50aa71c296d465a47e620f462dd5a5b56
halmoallim <halmoallim@mrsool.co>
2022-02-17 16:47:20 +0300

    cover & uploads
    done with adding the cover and uploads for the item model

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -68,6 +68,6 @@ class ItemsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def item_params
-      params.require(:item).permit(:name, :price)
+      params.require(:item).permit(:name, :price, :cover, uploads: [])
     end
 end
